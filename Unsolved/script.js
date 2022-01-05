539c2d6557505c4a3188545368761ea59420c53b
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-01-04 21:13:48 -0500

    initial commit

---
+++ Unsolved/script.js
@@ -0,0 +1,8 @@
+// TODO: Write Your JavaScript Code Here
+var students = ["Randall", "Rob", "Robert", "Ricky", "Rodrigo"]
+
+console.log(`${students.length}`);
+
+for (var i = 0; i < students.length; i++) {
+    console.log(`Great to see you, ${students[i]}`);
+}
